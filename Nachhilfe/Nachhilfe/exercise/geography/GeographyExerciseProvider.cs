400bb65745fd754e1a9570939437c70011f470dc
Jonas <Jonas@LAPTOP-T1SLSRQL>
2017-10-07 18:56:37 +0200

    Geography exercises created

---
+++ Nachhilfe/Nachhilfe/exercise/geography/GeographyExerciseProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nachhilfe
+{
+    public class GeographyExerciseProvider : IExerciseProvider<GeographyExercise>
+    {
+        private Random random { get; }
+
+
+        public GeographyExerciseProvider(Random random)
+        {
+            this.random = random;
+        }
+
+
+        public GeographyExercise NextExercise()
+        {
+            IList<Capital> capitals = Capitals.GetCapitals();
+            int capitalIndex = random.Next(capitals.Count);
+            return new GeographyExercise(capitals[capitalIndex]);
+        }
+
+    }
+}
---
+++ Nachhilfe/Nachhilfe/exercise/geography/data/Capital.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nachhilfe
+{
+    public class Capital
+    {
+        public string country { get; set; }
+        public double lat { get; set; }
+        public double lgn { get; set; }
+        public string name { get; set; }
+    }
+}
